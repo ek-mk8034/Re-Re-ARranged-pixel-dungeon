841677e18b4a74f0e61d1eb8c7197027a26731fd
Evan Debenham <Evan@ShatteredPixel.com>
2026-02-06 17:13:14 -0500

    v3.3.6: implemented two new hazards for the vault, some WIP still
    (cherry picked from commit cdec67e8d12d93ec9a280e631eecb46e0e15726d)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/VaultLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/VaultLevel.java
@@ -23,6 +23,8 @@ package com.shatteredpixel.shatteredpixeldungeon.levels;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.VaultFlameTraps;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
@@ -34,7 +36,9 @@ import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.levels.builders.Builder;
 import com.shatteredpixel.shatteredpixeldungeon.levels.builders.GridBuilder;
 import com.shatteredpixel.shatteredpixeldungeon.levels.features.LevelTransition;
+import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.Room;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.AlternatingTrapsRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultCircleRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultCrossRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultEntranceRoom;
@@ -42,6 +46,12 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultFi
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultLongRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultQuadrantsRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultRingRoom;
+<<<<<<< HEAD
+=======
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultRingsRoom;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultSimpleEnemyTreasureRoom;
+import com.shatteredpixel.shatteredpixeldungeon.levels.traps.Trap;
+>>>>>>> cdec67e8d (v3.3.6: implemented two new hazards for the vault, some WIP still)
 import com.watabou.utils.Random;
 
 import java.util.ArrayList;
@@ -59,6 +69,13 @@ public class VaultLevel extends CityLevel {
 			initRooms.add(new VaultCircleRoom());
 			initRooms.add(new VaultCrossRoom());
 			initRooms.add(new VaultQuadrantsRoom());
+<<<<<<< HEAD
+=======
+			initRooms.add(new VaultEnemyCenterRoom());
+			initRooms.add(new VaultRingsRoom());
+			initRooms.add(new VaultSimpleEnemyTreasureRoom());
+			initRooms.add(new AlternatingTrapsRoom());
+>>>>>>> cdec67e8d (v3.3.6: implemented two new hazards for the vault, some WIP still)
 		}
 
 		initRooms.add(new VaultLongRoom());
@@ -137,4 +154,30 @@ public class VaultLevel extends CityLevel {
 	public int randomRespawnCell( Char ch ) {
 		return entrance()-width();
 	}
+
+	public static class VaultFlameTrap extends Trap {
+
+		{
+			color = BLACK;
+			shape = DOTS;
+
+			canBeHidden = false;
+			active = false;
+		}
+
+		@Override
+		public void activate() {
+			//does nothing, this trap is just decoration and is always deactivated
+		}
+
+		public static void setupTrap(Level level, int cell, int initialDelay, int cooldown){
+			VaultFlameTraps traps = Blob.seed(0, 0, VaultFlameTraps.class, level);
+			traps.initialCooldowns[cell] = cooldown;
+			traps.cooldowns[cell] = initialDelay;
+			level.setTrap(new VaultLevel.VaultFlameTrap().reveal(), cell);
+			Painter.set(level, cell, Terrain.INACTIVE_TRAP);
+		}
+
+	}
+
 }
