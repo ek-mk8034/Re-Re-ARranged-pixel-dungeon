d75f58e03be74d46abd60e2221d002f0285c165b
Evan Debenham <Evan@ShatteredPixel.com>
2026-02-07 19:07:17 -0500

    v3.3.6: added a laser sentry hazard and a room for it
    (cherry picked from commit 28e1fe139f7825c4cd6fe5c7610c5c12e864cb25)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/VaultLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/VaultLevel.java
@@ -43,6 +43,7 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultCi
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultCrossRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultEntranceRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultFinalRoom;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultLasersRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultLongRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultQuadrantsRoom;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.VaultRingRoom;
@@ -73,6 +74,7 @@ public class VaultLevel extends CityLevel {
 			initRooms.add(new VaultRingsRoom());
 			initRooms.add(new VaultSimpleEnemyTreasureRoom());
 			initRooms.add(new AlternatingTrapsRoom());
+			initRooms.add(new VaultLasersRoom());
 		}
 
 		initRooms.add(new VaultLongRoom());
